093b3768868940abc6459f025d0a30c5157d0ee1
billyfish <tyrrell.simon@gmail.com>
2024-09-10 15:11:53 +0100

    updated and now installs apache, mongo, pcre2 and grassroots repos

--- unix/install_grassroots.sh
+++ unix/install_grassroots.sh
@@ -8,37 +8,84 @@ APR_UTIL_VER=1.6.3
 MONGODB_VER=7.0.14
 MONGODB_TOOLS_VER=100.10.0
 
+PCRE2_INSTALL_DIR=/opt/pcre2
+APACHE_INSTALL_DIR=/opt/apache
+MONGODB_INSTALL_DIR=/opt/mongodb
+
+
+
+declare -A grassroots_services
+grassroots_services[field-trials]="https://github.com/TGAC/grassroots-service-field-trial.git"
+grassroots_services[search]="https://github.com/TGAC/grassroots-service-search.git"
+grassroots_services[marti]="https://github.com/TGAC/grassroots-service-marti.git"
+
+grassroots_services[users-groups]="https://github.com/TGAC/grassroots-service-users-groups.git"
+grassroots_services[gene-trees]="https://github.com/TGAC/grassroots-service-gene-trees.git"
+grassroots_services[parental-genotype]="https://github.com/TGAC/grassroots-parental-genotype-service.git"
+
+grassroots_services[samtools]="https://github.com/TGAC/grassroots-service-samtools.git"
+grassroots_services[blast]="https://github.com/TGAC/grassroots-service-blast.git"
+grassroots_services[field-pathogenomics]="https://github.com/TGAC/grassroots-service-field-pathogenomics.git"
+
+
+declare -A grassroots_libs
+grassroots_libs[geocoder]="https://github.com/TGAC/grassroots-geocoder.git"
+grassroots_libs[frictionless-data]="https://github.com/TGAC/grassroots-frictionless-data.git"
+
+
+declare -A grassroots_servers
+grassroots_servers[httpd-server]="https://github.com/TGAC/grassroots-server-apache-httpd.git"
+grassroots_servers[mongodb-jobs-manager]="https://github.com/TGAC/grassroots-jobs-manager-mongodb.git"
+grassroots_servers[simple-servers-manager]="https://github.com/TGAC/grassroots-simple-servers-manager.git"
+grassroots_servers[brapi-module]="https://github.com/TGAC/grassroots-brapi-module.git"
+
+
+declare -A grassroots_clients
+
+declare -A grassroots_handlers
+
 
 #sudo apt install default-jdk libcurl4-openssl-dev gcc wget automake unzip bzip2 flex make git cmake zlib1g-dev g++ libzstd-dev libssl-dev libexpat1-dev
 
 GetAndUnpackArchive() {
 	name=$1
 	file_url=$2
+	suffix=${3:-tar.gz}
 
 	if [ ! -d $name ]; then
 		echo "creating $name"
 
-		if [ ! -f $name.tar.gz ]; then
-			echo "downloading $file_url/$name.tar.gz"
-			wget $file_url/$name.tar.gz
+		if [ ! -f $name.$suffix ]; then
+			echo "downloading $file_url/$name.$suffix"
+			wget $file_url/$name.$suffix
 
-			echo "unpacking $name.tar.gz"
-			tar zxf $name.tar.gz		
+			echo "unpacking $name.$suffix"
+			tar zxf $name.$suffix
 		fi
 	fi
 }
 
 
-GetGitRepo() {
-	name=$1
-	git_url=$2
+GetAllGitRepos() {
+	local -n data_ref=$1
 
-	if [ ! -d $name ]; then
-		echo "cloning $name"
-		git clone $git_repo $name	
-	fi
+	for key in ${!data_ref[@]}
+	do
+		echo "Calling GetGitRepo 1:${data_ref[${key}]} 2:${key}"
+		GetGitRepo ${data_ref[$key]} $key
+	done
 }
 
+
+GetGitRepo() {
+	git_url=$1
+	name=$2
+
+	if [ ! -d $name ]; then
+		echo "calling: git clone $git_url $name"
+		git clone $git_url $name
+	fi
+}
 
 EnsureDir() {
 	if [ ! -d $1 ]; then
@@ -62,18 +109,17 @@ EnsureDir ~/Downloads
 
 cd ~/Downloads
 
-echo "PCRE2_VER $PCRE2_VER"
 
 # Install PCRE2
-if [ ! -d "/opt/pcre2" ]; then
+if [ ! -d "$PCRE2_INSTALL_DIR" ]; then
 	GetAndUnpackArchive pcre2-$PCRE2_VER https://github.com/PCRE2Project/pcre2/releases/download/pcre2-$PCRE2_VER
 	cd pcre2-$PCRE2_VER
-	./configure --prefix=/opt/pcre2
+	./configure --prefix=$PCRE2_INSTALL_DIR
 	make
-	
-	echo "About to run: sudo EnsureDir /opt/pcre2"
-	SudoEnsureDir /opt/pcre2
-	
+
+	echo "About to run: SudoEnsureDir $PCRE2_INSTALL_DIR"
+	SudoEnsureDir $PCRE2_INSTALL_DIR
+
 	echo "installing pcre2"
 	make install
 fi
@@ -82,7 +128,7 @@ fi
 cd ~/Downloads
 
 # Install Apache
-if [ ! -d "/opt/apache" ]; then
+if [ ! -d "$APACHE_INSTALL_DIR" ]; then
 	GetAndUnpackArchive httpd-$HTTPD_VER https://dlcdn.apache.org/httpd/
 	GetAndUnpackArchive apr-$APR_VER https://dlcdn.apache.org/apr/
 	GetAndUnpackArchive apr-util-$APR_UTIL_VER https://dlcdn.apache.org/apr/
@@ -92,9 +138,9 @@ if [ ! -d "/opt/apache" ]; then
 	mv apr-$APR_VER httpd-$HTTPD_VER/srclib/apr
 	mv apr-util-$APR_UTIL_VER httpd-$HTTPD_VER/srclib/apr-util
 	cd httpd-$HTTPD_VER
-	./configure --prefix=/opt/apache --with-included-apr --with-pcre=/opt/pcre2/bin/pcre2-config
+	./configure --prefix=$APACHE_INSTALL_DIR --with-included-apr --with-pcre=$PCRE2_INSTALL_DIR/bin/pcre2-config
 	make
-	sudo EnsureDir /opt/apache
+	SudoEnsureDir $APACHE_INSTALL_DIR
 	make install
 fi
 
@@ -102,17 +148,20 @@ fi
 cd ~/Downloads
 
 # Install MongoDB
-if [ ! -d "/opt/mongodb" ]; then
-	GetAndUnpackArchive mongodb-linux-x86_64-ubuntu2204-$MONGODB_VER https://fastdl.mongodb.org/linux/
-	GetAndUnpackArchive mongodb-database-tools-ubuntu2204-x86_64-$MONGODB_TOOLS_VER https://fastdl.mongodb.org/tools/db/
+if [ ! -d "$MONGODB_INSTALL_DIR" ]; then
+	tools_name=mongodb-database-tools-ubuntu2404-x86_64-$MONGODB_TOOLS_VER
+	mongo_name=mongodb-linux-x86_64-ubuntu2204-$MONGODB_VER
 
-	tar zxf mongodb-linux-x86_64-ubuntu2204-$MONGODB_VER.tgz 
-	tar zxf mongodb-database-tools-ubuntu2204-x86_64-$MONGODB_TOOLS_VER.tgz 
+	GetAndUnpackArchive $mongo_name https://fastdl.mongodb.org/linux tgz
+	GetAndUnpackArchive $tools_name https://fastdl.mongodb.org/tools/db tgz
 
-	sudo EnsureDir /opt/mongodb
-	cp -r mongodb-database-tools-ubuntu2204-x86_64-$MONGODB_TOOLS_VER/* /opt/mongodb/
-	cp -r mongodb-linux-x86_64-ubuntu2204-$MONGODB_VER/* /opt/mongodb/
-	mkdir /opt/mongodb/dbs
+	tar zxf $mongo_name.tgz
+	tar zxf $tools_name.tgz
+
+	SudoEnsureDir $MONGODB_INSTALL_DIR
+	cp -r $tools_name/* $MONGODB_INSTALL_DIR
+	cp -r $mongo_name/* $MONGODB_INSTALL_DIR
+	mkdir $MONGODB_INSTALL_DIR/dbs
 fi
 
 
@@ -120,33 +169,29 @@ fi
 EnsureDir ~/Projects
 EnsureDir ~/Projects/grassroots
 
-cd ~/Projects/grassroots
-
 # Install Grassroots
-
+cd ~/Projects/grassroots
 
 GetGitRepo https://github.com/TGAC/grassroots-build-tools.git build-config
 GetGitRepo https://github.com/TGAC/grassroots-core.git core
 GetGitRepo https://github.com/TGAC/grassroots-lucene.git lucene
 
-EnsureDir clients
+EnsureDir ~/Projects/grassroots/services
+cd ~/Projects/grassroots/services
+GetAllGitRepos grassroots_services
 
-EnsureDir handlers
+EnsureDir ~/Projects/grassroots/servers
+cd ~/Projects/grassroots/servers
+GetAllGitRepos grassroots_servers
 
-EnsureDir libs
-GetGitRepo https://github.com/TGAC/grassroots-geocoder.git geocoder
-GetGitRepo https://github.com/TGAC/grassroots-frictionless-data.git frictionless-data
+EnsureDir ~/Projects/grassroots/libs
+cd ~/Projects/grassroots/libs
+GetAllGitRepos grassroots_libs
 
-EnsureDir services
-git clone https://github.com/TGAC/grassroots-service-field-trial.git field-trials
+EnsureDir ~/Projects/grassroots/clients
+cd ~/Projects/grassroots/clients
+GetAllGitRepos grassroots_clients
 
-EnsureDir servers
-cd servers
-GetGitRepo https://github.com/TGAC/grassroots-server-apache-httpd.git httpd-server
-GetGitRepo https://github.com/TGAC/grassroots-jobs-manager-mongodb.git mongodb-jobs-manager
-GetGitRepo https://github.com/TGAC/grassroots-simple-servers-manager.git simple-servers-manager
-
-
-
-
-
+EnsureDir ~/Projects/grassroots/handlers
+cd ~/Projects/grassroots/handlers
+GetAllGitRepos grassroots_handlers
